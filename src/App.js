59638cb507448683e09cef2e683ed57b9c1017bb
jihyuunn <791004_@likelion.org>
2019-09-10 09:25:05 +0900

    todo list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
 
 function App() {
+  const [todolist, setTodolist] = useState([]);
+  useEffect(() => {
+    const getList = async () => {
+      try {
+        const response = await Axios.get(
+          " http://localhost:8000/api/todolists"
+        );
+        console.log(response.data.data);
+        setTodolist(response.data.data)
+        // setTodolist(response.data.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getList();
+  }, []);
+  const lists = todolist.map((list, index) => <li key={index}>{list.content}</li>);
+
+  // const [todos, setTodos] = useState([])
+  // useEffect(() => {
+  //   setTodos([todolist.map(list => list.content)])
+  //   console.log(todos)
+  // }, [])
+
+  const [todoThing, setTodoThing] = useState("")
+  const getTodo = e => {
+    setTodoThing(e.target.value)
+  }
+  const addTodo = async () => {
+    try {
+      const response = await Axios.post(
+        " http://localhost:8000/api/todolists",
+        { "content":todoThing }
+      )
+      console.log(response)
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>{lists}</ul>
+      <form>
+        <label>todo</label>
+        <input value={todoThing} onChange={getTodo}></input>
+        <button onClick={addTodo}>add it</button>
+      </form>
     </div>
   );
 }
